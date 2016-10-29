34a102a6a15496ad78b97550dbef5fe96d4557f3
Dmitriy Shishkin <shishkindimon@gmail.com>
2016-10-29 05:49:13 +0300

    Merge BeliefSetConsistencyTester into ConsistencyTester

--- plugins/net.sf.tweety.logics.commons/src/net/sf/tweety/logics/commons/analysis/NaiveMusEnumerator.java
+++ plugins/net.sf.tweety.logics.commons/src/net/sf/tweety/logics/commons/analysis/NaiveMusEnumerator.java
@@ -34,13 +34,13 @@ import net.sf.tweety.commons.Formula;
 public class NaiveMusEnumerator<S extends Formula> implements MusEnumerator<S>{
 	
 	/** Used for making consistency checks. */
-	private BeliefSetConsistencyTester<S> tester;
+	private ConsistencyTester<S> tester;
 	
 	/**
 	 * Creates a new naive MusEnumerator that uses the given consistency tester.
 	 * @param tester some consistency tester
 	 */
-	public NaiveMusEnumerator(BeliefSetConsistencyTester<S> tester){
+	public NaiveMusEnumerator(ConsistencyTester<S> tester){
 		this.tester = tester;
 	}
 	

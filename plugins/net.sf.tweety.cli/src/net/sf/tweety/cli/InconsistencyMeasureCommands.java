5f89d4abf9dde7d13839d73f111e126f7cd7c680
Dmitriy Shishkin <shishkindimon@gmail.com>
2016-11-08 13:51:50 +0300

    Extract tests and examples from net.sf.tweety.cli
    * Rename source folder to "src";
    * Clean .classpath file
    * To fix #21;
    
    # Conflicts:
    #	plugins/net.sf.tweety.cli/.classpath
    #	plugins/net.sf.tweety.cli/src/net/sf/tweety/cli/BeliefBaseCommands.java
    #	plugins/net.sf.tweety.cli/src/net/sf/tweety/cli/BeliefBaseMachineShopCommands.java
    #	plugins/net.sf.tweety.cli/src/net/sf/tweety/cli/BeliefBaseSamplerCommands.java
    #	plugins/net.sf.tweety.cli/src/net/sf/tweety/cli/ConsistencyTesterCommands.java
    #	plugins/net.sf.tweety.cli/src/net/sf/tweety/cli/InconsistencyMeasureCommands.java
    #	plugins/net.sf.tweety.cli/src/net/sf/tweety/cli/ParserCommands.java
    #	plugins/net.sf.tweety.cli/src/net/sf/tweety/cli/ReasonerCommands.java

---
+++ plugins/net.sf.tweety.cli/src/net/sf/tweety/cli/InconsistencyMeasureCommands.java
@@ -0,0 +1,58 @@
+package net.sf.tweety.cli;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
+import org.apache.felix.service.command.Descriptor;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+import net.sf.tweety.logics.commons.analysis.InconsistencyMeasure;
+
+
+// @formatter:off
+@Component(
+	service = InconsistencyMeasureCommands.class,
+	property = { 
+		"osgi.command.scope:String=tweety", 
+		"osgi.command.function:String=measures",
+		"osgi.command.function:String=measure"
+})
+//@formatter:on
+public class InconsistencyMeasureCommands {
+
+	private final List<ServiceReference<InconsistencyMeasure<?>>> measures = new CopyOnWriteArrayList<>();
+
+	@Reference(service = InconsistencyMeasure.class, cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+	void bindInconsistencyMeasure(ServiceReference<InconsistencyMeasure<?>> reasoner) {
+		measures.add(reasoner);
+	}
+
+	void unbindInconsistencyMeasure(ServiceReference<InconsistencyMeasure<?>> reasoner) {
+		measures.remove(reasoner);
+	}
+
+	@Descriptor("get all parsers")
+	public void measures() {
+		if (measures.isEmpty()){
+			System.out.println("There is no Belief Base Inconsistency Measures registered");
+		} else {
+			System.out.println("ID	Belief Base Inconsistency Measures");
+		}
+		IntStream
+				.range(0, measures.size())
+				.mapToObj(i -> i + "	" + measures.get(i).getProperty("component.name"))
+				.forEach(System.out::println);
+	}
+
+	@Descriptor("get parser by id")
+	public InconsistencyMeasure<?> measure(int index) {
+		ServiceReference<InconsistencyMeasure<?>> reference = measures.get(index);
+		return reference.getBundle().getBundleContext().getService(reference);
+	}
+
+}
